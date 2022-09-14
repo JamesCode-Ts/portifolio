81bc353d3afbc6d995e890c10c66476535cc17a0
JamesCode-Ts@github.com <jameslob42@gmail.com>
2022-09-14 01:07:16 -0300

    script index

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const port = process.env.PORT || 3000;
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))});
 
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname + '/bio.html'))});
+
 
 app.listen(port, function () {
     console.info("Aplicação rodando em http://localhost:3000")
